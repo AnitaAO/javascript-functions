126fd0015154085dce5a18a1993b006dbd004dfc
Amarachi Ajah     -     Software Developer | UX/UI Designer <anitaisbeta@gmail.com>
2022-04-04 10:07:56 +0100

    first commit

---
+++ app.js
@@ -0,0 +1,11 @@
+function greetings() {
+    return 'Hello John';
+}
+let message = greetings();
+console.log(message);
+
+function sum(num1, num2){
+    return num1 + num2;
+}
+let result = sum(2);
+    console.log(result);
