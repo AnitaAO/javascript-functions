ea301c112c35432ebf5242eb05133ef9e3fee586
Amarachi Ajah     -     Software Developer | UX/UI Designer <anitaisbeta@gmail.com>
2022-04-04 11:32:09 +0100

    update

--- app.js
+++ app.js
@@ -1,20 +1,72 @@
-function greetings() {
-    return 'Hello John';
-}
-let message = greetings();
-console.log(message);
+// //--1--//
+// function greetings() {
+//     return 'Hello John';
+// }
+// let message = greetings();
+// console.log(message);
 
-function sum(num1, num2){
-    return num1 + num2;
-}
-let result = sum(2);
-    console.log(result);
+// // --2--// 
+// function sum(num1, num2){
+//     return num1 + num2;
+// }
+// let result = sum(2);
+//     console.log(result);
 
-function printAll(){
-    for(let i = 0; i < arguments.length; i++)
-    {
-        console.log(arguements[i]);
-    }
+// // --3--// 
+// function printAll(){
+//     for(let i = 0; i < arguments.length; i++)
+//     {
+//         console.log(arguements[i]);
+//     }
+// }
+// printAll(1, 2, 3, 4, 5);
+// printAll(10, 20);
+
+// // --4--//
+// function greeting(){
+//     let message = 'Hey';
+//     // --nested but undefined function-
+//     let sayHello = function hello() {
+//         console.log(message);
+//     };
+//     sayHello();
+// } 
+// greeting();
+
+// // --nested & defined--//
+// function greeting(){
+//     let message = 'hey';
+//     let sayHello = function hello(){
+//         let message = 'Hello';
+//     };
+//     sayHello();
+//     console.log(message);
+// }
+// greeting();
+
+// // --nested & defined and print nested function variable--//
+// function greeting(){
+//     let message = 'hey';
+//     let sayHello = function hello(){
+//         message = 'Hello';
+//     };
+//     sayHello();
+//     console.log(message);
+// }
+// greeting();
+
+
+// block scope
+let message = "Hello";
+if (message === "Hello") {
+    let count = 100;
 }
-printAll(1, 2, 3, 4, 5);
-printAll(10, 20);
+console.log(count); //error
+
+// Inside if block
+let message = 'Hello';
+if (message === 'Hello'){
+    let message = 'Inside if block';
+    console.log(message); //inside if block
+}
+console.log(message); //Hello
