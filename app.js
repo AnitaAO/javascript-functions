d3c88337da098f6d4010c1e76e951676ab3dbd0d
Amarachi Ajah     -     Software Developer | UX/UI Designer <anitaisbeta@gmail.com>
2022-04-04 10:23:53 +0100

    update

--- app.js
+++ app.js
@@ -9,3 +9,12 @@ function sum(num1, num2){
 }
 let result = sum(2);
     console.log(result);
+
+function printAll(){
+    for(let i = 0; i < arguments.length; i++)
+    {
+        console.log(arguements[i]);
+    }
+}
+printAll(1, 2, 3, 4, 5);
+printAll(10, 20);
